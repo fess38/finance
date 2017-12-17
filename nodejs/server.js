6bfa61476abcb6eb83f0152b5110c3d9fa5870f5
Yury Anapolskiy <fess38@gmail.com>
2017-12-17 19:45:09 +0300

    Https проксирование до бэкенда

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var httpProxy = require('http-proxy');
 var bodyParser = require('body-parser');
 
-httpProxy.prototype.onError = function (err) {
+httpProxy.prototype.onError = function(err) {
   console.log(err);
 };
 var apiProxy = httpProxy.createProxyServer({ changeOrigin: true });
@@ -12,16 +12,16 @@ server.set('port', process.env.PORT || 4200);
 server.use(express.static(__dirname + '/dist'));
 
 server.all("/api/*", function(req, res) {
-    apiProxy.web(req, res, { target: process.env.backend_url });
+  apiProxy.web(req, res, { target: process.env.backend_url, secure: true });
 });
 
-server.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
+server.get('*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
 server.listen(server.get('port'), function() {
-    console.log('Express server listening on port ' + server.get('port'));
+  console.log('Express server listening on port ' + server.get('port'));
 });
