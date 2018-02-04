499d7dd58d89330f45b05b8ad513a642f7884c08
Yury Anapolskiy <fess38@gmail.com>
2018-02-04 17:23:56 +0300

    Delete unused part of import.sql

--- kotlin/src/main/resources/import.sql
+++ kotlin/src/main/resources/import.sql
@@ -1,12 +1,1 @@
 CREATE SCHEMA IF NOT EXISTS model;
-/*
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'account_user_fk') THEN
-        ALTER TABLE model.account
-        ADD CONSTRAINT account_user_fk
-        FOREIGN KEY (user_id) REFERENCES model.user(id);
-    END IF;
-END;
-$$;
-*/
