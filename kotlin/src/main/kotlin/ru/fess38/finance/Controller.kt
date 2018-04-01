127b0b3638d3ca40c06913643370e0f45bd2ad50
Yury Anapolskiy <fess38@gmail.com>
2018-04-01 16:30:04 +0300

    Write http endpoints for all current entities
    - write separated validation class

--- kotlin/src/main/kotlin/ru/fess38/finance/Controller.kt
+++ kotlin/src/main/kotlin/ru/fess38/finance/Controller.kt
@@ -1,5 +1,6 @@
 package ru.fess38.finance
 
+import com.google.protobuf.Message
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.fess38.finance.model.Model.Account
+import ru.fess38.finance.model.Model.Category
 import ru.fess38.finance.model.Model.FamilyMember
-import ru.fess38.finance.validator.AccountValidator
-import ru.fess38.finance.validator.FamilyMamberValidator
+import ru.fess38.finance.model.Model.SubCategory
+import ru.fess38.finance.model.Model.Transaction
 
 @RestController
 @RequestMapping(
@@ -28,130 +30,105 @@ class UserDataController {
 
 @RestController
 @RequestMapping(
-    path = ["/api/data/account"],
+    path = ["/api/data"],
     produces = ["application/x-protobuf"],
     consumes = ["application/x-protobuf"]
 )
-class AccountController {
+class Controller {
   @Autowired
   lateinit var entityDao: EntityDao
 
-  val validator = AccountValidator()
+  val validator = InputValuesValidator()
 
-  @PostMapping("save")
-  fun save(@RequestBody account: Account): ResponseEntity<Any> {
+  private fun saveMessage(value: Message): ResponseEntity<Any> {
     var httpStatus: HttpStatus
-    var savedAccount = account
+    var savedValue = value
 
-    if (validator.isValid(account)) {
+    if (validator.isValid(value)) {
       httpStatus = HttpStatus.OK
       try {
-        savedAccount = entityDao.save(account) as Account
+        savedValue = entityDao.save(value)
       } catch (e: Exception) {
         httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
       }
     } else {
       httpStatus = HttpStatus.BAD_REQUEST
     }
-    return ResponseEntity(savedAccount, httpStatus)
+    return ResponseEntity(savedValue, httpStatus)
   }
 
-  @PostMapping("update")
-  fun update(@RequestBody account: Account): ResponseEntity<Any> {
+  @PostMapping("account/save")
+  fun save(@RequestBody value: Account) = saveMessage(value)
+
+  @PostMapping("family_member/save")
+  fun save(@RequestBody value: FamilyMember) = saveMessage(value)
+
+  @PostMapping("category/save")
+  fun save(@RequestBody value: Category) = saveMessage(value)
+
+  @PostMapping("sub_category/save")
+  fun save(@RequestBody value: SubCategory) = saveMessage(value)
+
+  @PostMapping("transaction/save")
+  fun save(@RequestBody value: Transaction) = saveMessage(value)
+
+  private fun updateMessage(value: Message, id: Long): ResponseEntity<Any> {
     var httpStatus: HttpStatus
-    val accountOld = entityDao.findById(id = account.id) as Account?
-    if (validator.isValid(account, accountOld)) {
+    if (validator.isValid(value, entityDao.findById(id = id))) {
       httpStatus = HttpStatus.OK
       try {
-        entityDao.update(account)
+        entityDao.update(value)
       } catch (e: Exception) {
         httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
       }
     } else {
       httpStatus = HttpStatus.BAD_REQUEST
     }
-    return ResponseEntity(account, httpStatus)
+    return ResponseEntity(value, httpStatus)
   }
 
-  @PostMapping("delete")
-  fun delete(@RequestBody account: Account): ResponseEntity<Any> {
+  @PostMapping("account/update")
+  fun update(@RequestBody value: Account) = updateMessage(value, value.id)
+
+  @PostMapping("category/update")
+  fun update(@RequestBody value: Category) = updateMessage(value, value.id)
+
+  @PostMapping("sub_category/update")
+  fun update(@RequestBody value: SubCategory) = updateMessage(value, value.id)
+
+  @PostMapping("family_member/update")
+  fun update(@RequestBody value: FamilyMember) = updateMessage(value, value.id)
+
+  @PostMapping("transaction/update")
+  fun update(@RequestBody value: Transaction) = updateMessage(value, value.id)
+
+  fun deleteMessage(value: Message, id: Long): ResponseEntity<Any> {
     var httpStatus: HttpStatus
-    val accountOld = entityDao.findById(id = account.id) as Account?
-    if (validator.isValid(account, accountOld)) {
+    if (validator.isValid(value, entityDao.findById(id = id))) {
       httpStatus = HttpStatus.OK
       try {
-        entityDao.delete(account)
+        entityDao.delete(value)
       } catch (e: Exception) {
         httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
       }
     } else {
       httpStatus = HttpStatus.BAD_REQUEST
     }
-    return ResponseEntity(account, httpStatus)
-  }
-}
-
-@RestController
-@RequestMapping(
-    path = ["/api/data/family_member"],
-    produces = ["application/x-protobuf"],
-    consumes = ["application/x-protobuf"]
-)
-class FamilyMemberController {
-  @Autowired
-  lateinit var entityDao: EntityDao
-
-  val validator = FamilyMamberValidator()
-
-  @PostMapping("save")
-  fun save(@RequestBody familyMember: FamilyMember): ResponseEntity<Any> {
-    var httpStatus: HttpStatus
-    var savedFamilyMember = familyMember
-
-    if (validator.isValid(familyMember)) {
-      httpStatus = HttpStatus.OK
-      try {
-        savedFamilyMember = entityDao.save(familyMember) as FamilyMember
-      } catch (e: Exception) {
-        httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
-      }
-    } else {
-      httpStatus = HttpStatus.BAD_REQUEST
-    }
-    return ResponseEntity(savedFamilyMember, httpStatus)
+    return ResponseEntity(value, httpStatus)
   }
 
-  @PostMapping("update")
-  fun update(@RequestBody familyMember: FamilyMember): ResponseEntity<Any> {
-    var httpStatus: HttpStatus
-    val familyMemberOld = entityDao.findById(id = familyMember.id) as FamilyMember?
-    if (validator.isValid(familyMember, familyMemberOld)) {
-      httpStatus = HttpStatus.OK
-      try {
-        entityDao.update(familyMember)
-      } catch (e: Exception) {
-        httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
-      }
-    } else {
-      httpStatus = HttpStatus.BAD_REQUEST
-    }
-    return ResponseEntity(familyMember, httpStatus)
-  }
+  @PostMapping("account/delete")
+  fun delete(@RequestBody value: Account) = deleteMessage(value, value.id)
 
-  @PostMapping("delete")
-  fun delete(@RequestBody familyMember: FamilyMember): ResponseEntity<Any> {
-    var httpStatus: HttpStatus
-    val familyMemberOld = entityDao.findById(id = familyMember.id) as FamilyMember?
-    if (validator.isValid(familyMember, familyMemberOld)) {
-      httpStatus = HttpStatus.OK
-      try {
-        entityDao.delete(familyMember)
-      } catch (e: Exception) {
-        httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
-      }
-    } else {
-      httpStatus = HttpStatus.BAD_REQUEST
-    }
-    return ResponseEntity(familyMember, httpStatus)
-  }
+  @PostMapping("category/delete")
+  fun delete(@RequestBody value: Category) = deleteMessage(value, value.id)
+
+  @PostMapping("sub_category/delete")
+  fun delete(@RequestBody value: SubCategory) = deleteMessage(value, value.id)
+
+  @PostMapping("family_member/delete")
+  fun delete(@RequestBody value: FamilyMember) = deleteMessage(value, value.id)
+
+  @PostMapping("transaction/delete")
+  fun delete(@RequestBody value: Transaction) = deleteMessage(value, value.id)
 }
