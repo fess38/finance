88168f46471fd94973ec17ee4eb87204dacbf2ff
Yury Anapolskiy <fess38@gmail.com>
2019-03-08 13:56:59 +0300

    Add filter by category in sub category list

--- nodejs/src/app/sub-category/sub-category-list/sub-category-list.component.ts
+++ nodejs/src/app/sub-category/sub-category-list/sub-category-list.component.ts
@@ -7,15 +7,29 @@ import { UserDataService } from '../../core/user-data/user-data.service';
   templateUrl: 'sub-category-list.component.html'
 })
 export class SubCategoryListComponent {
+  filterCategoryId: number;
+
   constructor(private userdata: UserDataService) {}
 
+  categories(): Category[] {
+    return _.chain(this.userdata.categories())
+      .filter(x => !x.isDeleted)
+      .sortBy(x => x.isIncome)
+      .sortBy(x => x.name.toLowerCase())
+      .value();
+  }
+
   subCategories(): SubCategory[] {
-    return _.chain(this.userdata.subCategories())
+    let result = _.chain(this.userdata.subCategories())
       .filter(x => !x.isDeleted)
       .sortBy(x => x.name.toLowerCase())
       .sortBy(x => this.findCategory(<number>x.categoryId).name)
       .sortBy(x => !this.findCategory(<number>x.categoryId).isIncome)
       .value();
+    if (this.filterCategoryId != null) {
+      result = result.filter(x => x.categoryId == this.filterCategoryId);
+    }
+    return result;
   }
 
   findCategory(categoryId: number): Category {
